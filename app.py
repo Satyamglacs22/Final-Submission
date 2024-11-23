fed4b6b92f973a765a28e70e888748cf1f772ce4
SATYAM SINGH <122612918+Satyamglacs22@users.noreply.github.com>
2024-11-23 20:35:49 +0530

    app.py

--- app.py
+++ app.py
@@ -17,13 +17,14 @@ st.subheader("Please Enter the Required Information:")
 # Input fields
 model_choice = st.selectbox('Select the Model', ['Logistic Regression'])
 
-age = st.number_input('Age', min_value=10, max_value=100, step=1)
-bmi = st.number_input('BMI', min_value=0.0, max_value=70.0, step=0.1)
-pregnancies = st.number_input('Pregnancies', min_value=0, max_value=20, step=1)
-glucose = st.number_input('Glucose', min_value=0, max_value=200, step=1)
-blood_pressure = st.number_input('Blood Pressure', min_value=20, max_value=200, step=1)
-skin_thickness = st.number_input('Skin Thickness', min_value=0, max_value=100, step=1)
-insulin = st.number_input('Insulin', min_value=0, max_value=900, step=1)
+Age = st.number_input('Age', min_value=10, max_value=100, step=1)
+BMI = st.number_input('BMI', min_value=0.0, max_value=70.0, step=0.1)
+Pregnancies = st.number_input('Pregnancies', min_value=0, max_value=20, step=1)
+Glucose = st.number_input('Glucose', min_value=0, max_value=200, step=1)
+BloodPressure = st.number_input('Blood Pressure', min_value=20, max_value=200, step=1)
+SkinThickness = st.number_input('Skin Thickness', min_value=0, max_value=100, step=1)
+Insulin = st.number_input('Insulin', min_value=0, max_value=900, step=1)
+DiabetesPedigreeFunction = st.number_input('Diabetes Pedigree Function', min_value=0.0, max_value=2.5, step=0.01)
 
 # Prediction
 diabetes_diagnosis = ''
